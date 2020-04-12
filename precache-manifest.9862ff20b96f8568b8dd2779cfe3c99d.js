afaffba36181f4b074c7b5f37836ca6e39cd6b5a
Tousif <tousifislampersonal@outlook.com>
2020-04-12 09:08:40 +0530

    Updates

---
+++ precache-manifest.9862ff20b96f8568b8dd2779cfe3c99d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ba3a04e1035ebd325700db73c954c51",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "2468ed843418d6b4d0e0",
+    "url": "/robofriends/static/js/2.071a3384.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/robofriends/static/js/2.071a3384.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82e61080b61ec70559df",
+    "url": "/robofriends/static/js/main.114502b5.chunk.js"
+  },
+  {
+    "revision": "dab07940c1980e5aab1e",
+    "url": "/robofriends/static/js/runtime-main.a4afcfec.js"
+  }
+]);
